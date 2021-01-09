ffc9e14281e5fb936cafadf359b612f66a02c8e7
Alex Shkil <alexhlins@gmail.com>
2021-01-09 17:55:00 +0300

    Excahge methods in bot_loading_utils

--- bot.py
+++ bot.py
@@ -8,12 +8,14 @@ bot = telebot.TeleBot(aps.token)
 
 @bot.message_handler(func = lambda message: message.text[0]=='/')
 def command_parse(message):
-    print('command_parse: ', ' from: ', message.chat.username,' command: ', message.text)
+    print('command_parse: ', ' from: ', message.chat.first_name,' command: ', message.text, ' text: ',aps.parse_command(message.text)[0] )
+    print(aps.bot_commands.keys())
+    bot.send_message(message.chat.id, aps.parse_command(message.text)[0])
     
 
 @bot.message_handler()
 def message_parse(message):
-    print('message_parse: ',message.text, ' from: ', message.chat.username)
-
+    print('message_parse: ',message.text, ' from: ', message.chat.first_name)
+    bot.send_message(message.chat.id, str(aps.parse_text(message.text)))
 
 bot.polling()
--- bot_loading_utils.py
+++ bot_loading_utils.py
@@ -8,39 +8,63 @@ class aps_bot(telebot.TeleBot):
     template_text_start = ''
     template_text_help = ''
     template_text_contacts = ''
+    template_text_dontunderstand = ''
+
+    # this method returns function from self.bot_commands dictionary by there str key
+    def parse_command(self, cmd):
+        
+        try:
+            func = self.bot_commands.get(cmd,None)
+        except:
+            return self.get_dontunderstand()
+        if func == None:
+            return self.get_dontunderstand()
+        return func(self)
+
+    # this method returns "reaction" for text, non command message
+    def parse_text(self, cmd):
+
+        return self.get_dontunderstand()[0]
     
 
+    '''
+        Module to return texts from templates:
+    '''
 
-    def get_start_message(self,name):
+    def get_start_message(self):
         
-        text = name + self.template_text_start
+        text = self.template_text_start
 
         return text
     
-    def get_help_message(self,name):
+    
+    def get_help_message(self):
         
-        text = name + self.template_text_help
+        text = self.template_text_help
 
         return text
     
-    def get_contacts_message(self,name):
+    
+    def get_contacts_message(self):
         
-        text = name + self.template_text_contacts
+        text = self.template_text_contacts
 
         return text
     
-
-
-    def __init__(self):
+    def get_dontunderstand(self):
         
-        if self.load_api_key():
-           Exception
-        self.load_template_text_contacts()
-        self.load_template_text_help()
-        self.load_template_text_start()
-        
+        text = self.template_text_dontunderstand
+
+        return text
+
+    '''
+        End of module to return texts from templates:
+    '''
+            
     def get_line_fromfile(self, filename):
+        
         result = ''
+        
         try:
             data_file = open(filename, mode='r')
             result = data_file.readline()
@@ -48,18 +72,47 @@ class aps_bot(telebot.TeleBot):
             return FileExistsError
         return result
 
+    
     def get_lines_fromfile(self, filename):
+        
         result = ''
+        
         try:
             data_file = open(filename, mode='r')
             result = data_file.readlines()
         except:
             return FileExistsError
         return result
+
+
+    '''
+        Module to load data from files:
+    '''
+
+    #loading text for /start command
+    def load_template_text_start(self):
+        
+        self.template_text_start = self.get_lines_fromfile('message_templates/start_message')
         
 
-    def load_api_key(self):
+    #loading text for /help command
+    def load_template_text_help(self):
         
+        self.template_text_help = self.get_lines_fromfile('message_templates/help_message')
+
+    #loading text for /contact command
+    def load_template_text_contacts(self):
+        
+        self.template_text_contacts = self.get_lines_fromfile('message_templates/contacts_message')
+    
+    #loading text for don't understand sentence 
+    def load_template_text_dontunderstand(self):
+       
+        self.template_text_dontunderstand = self.get_lines_fromfile('message_templates/sorry_im_dnt_understand')
+    
+    # loading token method
+    def load_api_key(self):
+    
         result = False
 
         try:
@@ -68,15 +121,29 @@ class aps_bot(telebot.TeleBot):
             return FileExistsError
 
         return result
-    
-    def load_template_text_start(self):
-        self.template_text_start = self.get_lines_fromfile('message_templates\\start_message')
-        
+  
+    '''
+        End of the module to load texts from templates:
+    '''    
 
-    def load_template_text_help(self):
-        self.template_text_help = self.get_lines_fromfile('message_templates\\help_message')
+    # list of commands aliases
+    bot_commands = {
+        '/help': get_help_message,
+        '/start': get_start_message,
+        '/contacts': get_contacts_message
+    }
 
-    def load_template_text_contacts(self):
-        self.template_text_contacts = self.get_lines_fromfile('message_templates\\contacts_message')
+    # just load all data from all templates
+    def load_data_from_templates(self):
+        self.load_template_text_contacts()
+        self.load_template_text_help()
+        self.load_template_text_start()
+        self.load_template_text_dontunderstand()
+
+    # main init class method
+    def __init__(self):       
+        if self.load_api_key():
+           Exception
+        self.load_data_from_templates()
 
     pass
